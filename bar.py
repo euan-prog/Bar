9155f22079ca55b1bf72bcab7a10cc0a78abdca4
Euan <euanchree@gmail.com>
2021-05-15 23:02:31 +0100

    Changed some stuff around

--- bar.py
+++ bar.py
@@ -7,8 +7,8 @@ fd = sys.stdout
 
 def getTitle(windowId):
 	title = subprocess.run("xprop -id " + windowId + " _OB_APP_GROUP_CLASS", shell = True, capture_output=True)
-	
-	if (title.returncode == 0):
+
+        if (title.returncode == 0):
 		title = title.stdout.decode("utf-8")
 		title = title[36:len(title)-2]
 		return title
@@ -17,8 +17,8 @@ def getTitle(windowId):
 
 def clock():
 	clock = subprocess.run('date "+%H:%M %d/%m/%y"', shell = True, capture_output = True)
-	
-	if (clock.returncode == 0):
+
+        if (clock.returncode == 0):
 		clock = clock.stdout.decode("utf-8").replace("\n","")
 		return clock
 	else:
@@ -26,18 +26,15 @@ def clock():
 
 def taskManager():
 	focwin = subprocess.run("xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5", shell = True, capture_output = True)
-	
-	if (focwin.returncode == 0):
+
+        if (focwin.returncode == 0):
 		focwin = focwin.stdout.decode("utf-8")
 	else:
 		return "Focused window can't be found"
-		
 	wins = subprocess.run("xprop -root _NET_CLIENT_LIST", shell = True, capture_output= True)
-	
 	if (wins.returncode == 0):
 		wins = wins.stdout.decode("utf-8")
 		wins = wins[38:len(wins)].replace("\n", "").replace("\n","").split(", ")
-	
 		out = ""
 
 		for index in range(0,len(wins)):
@@ -45,11 +42,11 @@ def taskManager():
 				out += getTitle(wins[index])
 			else:
 				out += "%{A:" + "wmctrl -a " + wins[index] + " -i:}" + str(getTitle(wins[index])) + "%{A} "
-			
+
 		return out.replace("\n","")
 	else:
 		return "task manager error"
-	
+
 while True:
 	output = "%{c}" + taskManager() + "%{r}" + clock() + "\n"
 	fd.write(output)
