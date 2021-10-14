d093097f0207ad19381d7578390485cf83f4cf0e
Euan Chree <euanchree@gmail.com>
2021-10-14 16:22:09 +0100

    More fixes

--- bar.py
+++ bar.py
@@ -1,4 +1,3 @@
-#!/bin/python
 #python bar.py | lemonbar -p | bash
 #Euan Chree 
 import sys, subprocess, time
@@ -8,7 +7,7 @@ fd = sys.stdout
 def getTitle(windowId):
 	title = subprocess.run("xprop -id " + windowId + " _OB_APP_GROUP_CLASS", shell = True, capture_output=True)
 
-        if (title.returncode == 0):
+	if (title.returncode == 0):
 		title = title.stdout.decode("utf-8")
 		title = title[36:len(title)-2]
 		return title
@@ -18,7 +17,7 @@ def getTitle(windowId):
 def clock():
 	clock = subprocess.run('date "+%H:%M %d/%m/%y"', shell = True, capture_output = True)
 
-        if (clock.returncode == 0):
+	if (clock.returncode == 0):
 		clock = clock.stdout.decode("utf-8").replace("\n","")
 		return clock
 	else:
@@ -27,12 +26,15 @@ def clock():
 def taskManager():
 	focwin = subprocess.run("xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5", shell = True, capture_output = True)
 
-        if (focwin.returncode == 0):
+	if (focwin.returncode == 0):
 		focwin = focwin.stdout.decode("utf-8")
 	else:
 		return "Focused window can't be found"
+
 	wins = subprocess.run("xprop -root _NET_CLIENT_LIST", shell = True, capture_output= True)
+
 	if (wins.returncode == 0):
+
 		wins = wins.stdout.decode("utf-8")
 		wins = wins[38:len(wins)].replace("\n", "").replace("\n","").split(", ")
 		out = ""
@@ -52,3 +54,4 @@ while True:
 	fd.write(output)
 	fd.flush()
 	time.sleep(0.1)
+
